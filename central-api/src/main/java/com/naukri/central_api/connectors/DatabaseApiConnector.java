293cd84ba5f5e096ee7532c5ea91f04d2eb64c36
Parul Pandey <parulss1606@gmail.com>
2025-06-21 00:25:25 +0530

    Optimized: Centralized REST logic

--- central-api/src/main/java/com/naukri/central_api/connectors/DatabaseApiConnector.java
+++ central-api/src/main/java/com/naukri/central_api/connectors/DatabaseApiConnector.java
@@ -3,6 +3,7 @@ package com.naukri.central_api.connectors;
 import com.naukri.central_api.models.AppUser;
 import com.naukri.central_api.models.Company;
 import com.naukri.central_api.models.Skill;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
@@ -10,47 +11,41 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+
 @Component
-public class DatabaseApiConnector {
+public class DatabaseApiConnector extends RestAPI{
     // we will write all calling methods here we will calling all the database api endpoints from this class
 
     @Value("${database.api.baseurl}")
     String baseUrl;
 
+    ModelMapper modelMapper = new ModelMapper();
+
     public Skill callGetSkillByNameEndpoint(String skillName){
         // In this function we will have logic to hit getSkillByName endpoint of DBAPI
         // Create URL
         String url = baseUrl + "/skill/get/" + skillName;
-        // creation of request
-        RequestEntity request = RequestEntity.get(url).build();
-        // Use restTemplate class to hit the api url
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Skill> response = restTemplate.exchange(url, HttpMethod.GET, request, Skill.class);
-        return response.getBody();
+        Object resp = this.makeGetCall(url, new HashMap<>());
+        return modelMapper.map(resp, Skill.class);
     }
 
     public AppUser callSaveUserEndpoint(AppUser user){
         String url = baseUrl + "/user/save";
-        RequestEntity request = RequestEntity.post(url).body(user);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<AppUser> response = restTemplate.exchange(url, HttpMethod.POST, request, AppUser.class);
-        return response.getBody();
+        Object resp = this.makePostCall(url, user, new HashMap<>());
+        return modelMapper.map(resp, AppUser.class);
 
     }
 
     public Skill callSaveSkillEndpoint(Skill skill){
         String url = baseUrl + "/skill/save";
-        RequestEntity request = RequestEntity.post(url).body(skill);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Skill> response = restTemplate.exchange(url, HttpMethod.POST, request, Skill.class);
-        return response.getBody();
+        Object resp = this.makePostCall(url, skill, new HashMap<>());
+        return modelMapper.map(resp, Skill.class);
     }
 
     public Company callSaveCompanyEndpoint(Company company){
         String url = baseUrl + "/company/save";
-        RequestEntity request = RequestEntity.post(url).body(company);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Company> response = restTemplate.exchange(url, HttpMethod.POST, request, Company.class);
-        return response.getBody();
+        Object resp = this.makePostCall(url, company, new HashMap<>());
+        return modelMapper.map(resp, Company.class);
     }
 }
